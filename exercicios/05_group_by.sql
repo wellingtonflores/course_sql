7ded910fff81fdc3c7553d1f22b0d2b025bfef24
Wellington Luis Cardoso Flores <wellington-flores@PCRS.INTRA.RS.GOV.BR>
2026-05-22 15:26:40 -0300

    exercicios de group_by

--- exercicios/05_group_by.sql
+++ exercicios/05_group_by.sql
@@ -1,10 +1,19 @@
 -- Qual o valor médio de pontos positivos por dia?
 
 SELECT sum(QtdePontos) AS totalpontos,
-        count(DISTINCT datetime(DtCriacao)) AS qtdeDiasUnicos
+        count(DISTINCT substr(DtCriacao, 1, 10)) AS qtdeDiasUnicos,
+        sum(QtdePontos) / count(DISTINCT substr(DtCriacao, 1, 10)) AS avgPontosDia
+
+FROM transacoes
+
+WHERE QtdePontos > 0;
+
+SELECT substr(DtCriacao, 1, 10) AS dtDia,
+        avg(QtdePontos) AS avgpontosDia
 
 FROM transacoes
 
 WHERE QtdePontos > 0
 
-GROUP BY DtCriacao
+GROUP BY 1
+ORDER BY 1;
