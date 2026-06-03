dcd7d5d1fa899fb410a468327c9e091c89f6e4ec
Wellington Luis Cardoso Flores <wellington-flores@PCRS.INTRA.RS.GOV.BR>
2026-06-03 12:17:48 -0300

    churn

--- exercicios/10_join_cte.sql
+++ exercicios/10_join_cte.sql
@@ -1,12 +1,41 @@
 -- 10. Como foi a curva de Churn do Curso de SQL?
 
+-- SELECT 
+--         substr(DtCriacao, 1, 10) AS dtDia,
+--         count(DISTINCT idCliente) AS qtdeClientes
+
+-- FROM transacoes
+
+-- WHERE DtCriacao >= '2025-08-25'
+-- AND DtCriacao < '2025-08-30'
+
+-- GROUP BY 1
+
+WITH tb_clientes_d1 AS (
+
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE DtCriacao >= '2025-08-25'
+    AND DtCriacao < '2025-08-26'
+),
+
+
+tb_join AS (
 SELECT 
-        substr(DtCriacao, 1, 10) AS dtDia,
-        count(DISTINCT idCliente) AS qtdeClientes
+        substr(t2.DtCriacao,1,10) AS dtDia,
+        count(DISTINCT t1.idCliente) AS qtdeCliente,
+        1.* count(DISTINCT t1.IdCliente) / (select count(*) from tb_clientes_d1)  AS pctRetencao,
+        1 - 1.* count(DISTINCT t1.IdCliente) / (select count(*) from tb_clientes_d1)  AS pctChurn
 
-FROM transacoes
+FROM tb_clientes_d1 AS t1
 
-WHERE DtCriacao >= '2025-08-25'
-AND DtCriacao < '2025-08-30'
+LEFT JOIN transacoes AS t2
+ON t1.idCliente = t2.idCliente
+
+WHERE t2.DtCriacao >= '2025-08-25'
+AND t2.DtCriacao < '2025-08-30'
 
 GROUP BY 1
+)
+
+SELECT * FROM tb_join
