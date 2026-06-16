91fd715af823609a5a03af868b4dd0979ba23c1c
Wellington Luis Cardoso Flores <wellington-flores@PCRS.INTRA.RS.GOV.BR>
2026-06-16 12:56:22 -0300

    finalizando o projeto

--- etl_projeto.sql
+++ etl_projeto.sql
@@ -4,7 +4,8 @@ WITH tb_transacoes AS (
             IdCliente,
             QtdePontos,
             datetime(substr(DtCriacao, 1, 19)) AS DtCriacao,
-            julianday('now') - julianday(substr(DtCriacao, 1, 10)) AS diffDate
+            julianday('now') - julianday(substr(DtCriacao, 1, 10)) AS diffDate,
+            CAST(strftime('%H', DtCriacao) AS INTEGER) AS dtHora
 
     FROM transacoes
 ),
@@ -90,10 +91,9 @@ SELECT *,
 FROM tb_cliente_produto
 ),
 
-
-
 tb_cliente_dia AS (
-SELECT IdCliente,
+
+SELECT idCliente,
         strftime('%w', DtCriacao) AS dtDia,
         count(*) AS qtdeTransacao
 
@@ -102,56 +102,86 @@ FROM tb_transacoes
 WHERE diffDate <= 28
 
 GROUP BY IdCliente, dtDia
-
 ),
 
 tb_cliente_dia_rn AS (
-
 SELECT *,
-    row_number() OVER (PARTITION BY IdCliente ORDER BY qtdeTransacao DESC) AS rnDia
-
+        ROW_NUMBER() OVER (PARTITION BY IdCliente ORDER BY qtdeTransacao DESC) AS rnDia 
 FROM tb_cliente_dia
+),
+
+tb_cliente_periodo AS (
+SELECT IdTransacao, 
+        IdCliente,
+        DtCriacao,
+        dtHora,
+        CASE 
+        WHEN dtHora BETWEEN 7 AND 12 THEN 'MANHÃ'
+        WHEN dtHora BETWEEN 13 AND 18 THEN 'TARDE'
+        WHEN dtHora BETWEEN 19 AND 23 THEN 'NOITE'
+        ELSE 'MADRUGADA'
+        END AS dtPeriodo,
+        count(*) AS qtdeTransacao
+        
+FROM tb_transacoes
+
+GROUP BY 1, 2
+),
+
+tb_cliente_periodo_rn AS (
+SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY IdCliente ORDER BY qtdeTransacao DESC) AS rnPeriodo
+
+FROM tb_cliente_periodo
 ),
 
 tb_join AS (
 
-    SELECT t1.*,
-            t2.idadeBase,
-            t3.DescNomeProduto AS produtoVida,
-            t4.DescNomeProduto AS produto56,
-            t5.DescNomeProduto AS produto28,
-            t6.DescNomeProduto AS produto14,
-            t7.DescNomeProduto AS produto7,
-            t8.dtDia
+SELECT t1.*,
+        t2.idadeBase,
+        t3.DescNomeProduto AS produtoVida,
+        t4.DescNomeProduto AS produto56,
+        t5.DescNomeProduto AS produto28,
+        t6.DescNomeProduto AS produto14,
+        t7.DescNomeProduto AS produto7,
+        COALESCE(t8.dtDia, -1) AS dtDia,
+        t9.dtPeriodo
 
-    FROM tb_sumario_transacoes AS t1
+FROM tb_sumario_transacoes AS t1
 
-    LEFT JOIN tb_cliente AS t2
-    ON t1.IdCliente = t2.IdCliente
+LEFT JOIN tb_cliente AS t2
+ON t1.IdCliente = t2.IdCliente
 
-    LEFT JOIN tb_cliente_produto_rn AS t3
-    ON t1.IdCliente = t3.IdCliente
-    AND t3.rnVida = 1
+LEFT JOIN tb_cliente_produto_rn AS t3
+ON t1.IdCliente = t3.IdCliente
+AND t3.rnVida = 1
 
-    LEFT JOIN tb_cliente_produto_rn AS t4
-    ON t1.IdCliente = t3.IdCliente
-    AND t3.rn56 = 1
+LEFT JOIN tb_cliente_produto_rn AS t4
+ON t1.IdCliente = t3.IdCliente
+AND t3.rn56 = 1
 
-    LEFT JOIN tb_cliente_produto_rn AS t5
-    ON t1.IdCliente = t3.IdCliente
-    AND t3.rn28 = 1
+LEFT JOIN tb_cliente_produto_rn AS t5
+ON t1.IdCliente = t3.IdCliente
+AND t3.rn28 = 1
 
-    LEFT JOIN tb_cliente_produto_rn AS t6
-    ON t1.IdCliente = t3.IdCliente
-    AND t3.rn14 = 1
+LEFT JOIN tb_cliente_produto_rn AS t6
+ON t1.IdCliente = t3.IdCliente
+AND t3.rn14 = 1
 
-    LEFT JOIN tb_cliente_produto_rn AS t7
-    ON t1.IdCliente = t3.IdCliente
-    AND t3.rn7 = 1
+LEFT JOIN tb_cliente_produto_rn AS t7
+ON t1.IdCliente = t3.IdCliente
+AND t3.rn7 = 1
 
-    LEFT JOIN tb_cliente_dia_rn AS t8
-    ON t1.IdCliente = t8.IdCliente
-    AND t8.rnDia = 1
+LEFT JOIN tb_cliente_dia_rn AS t8
+ON t1.IdCliente = t8.IdCliente
+AND t8.rnDia = 1
+
+LEFT JOIN tb_cliente_periodo_rn AS t9
+ON t1.IdCliente = t9.IdCliente
+AND t9.rnPeriodo = 1
 )
 
-SELECT * FROM tb_join
+SELECT IdCliente, dtDia, dtPeriodo
+FROM tb_join
+
+LIMIT 5
